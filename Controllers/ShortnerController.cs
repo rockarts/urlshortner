bd098282ec88737bfec0244f184b571fcd6b9096
Steven Rockarts <srockarts@invidi.com>
2016-08-08 15:48:13 -0600

    Working ghetto shortner

--- Controllers/ShortnerController.cs
+++ Controllers/ShortnerController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,9 +25,10 @@ namespace urlshortner.Controllers
         [HttpPost]
         public IActionResult Shorten(Url url)
         {
-            urlList.Add(url.ToString());
-            //url.Shorten();
-            return Redirect("Home/Index");
+            int key = urlList.Add(url.LongURL.ToString());
+            string shortUrl = url.Encode(key);
+            ViewData["ShortUrl"] = string.Format("Your Short URL is: http://localhost/{0}",  shortUrl);
+            return View("~/Views/Home/Index.cshtml");
             //return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
         }
 
